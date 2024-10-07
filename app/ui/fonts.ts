70295e5d18989d81bc7dee5509e028d609ff8b40
Jacyked <services@jacykdev.com>
2024-10-07 17:06:13 -0600

    Initial Commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,15 @@
+import { Inter, Lusitana, Montserrat, Roboto } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
+  export const montserrat = Montserrat({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
+  export const roboto = Roboto({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
