d64b876b5a45615d34c65fa5ca25a160042e444e
Jacyked <services@jacykdev.com>
2024-10-08 15:13:39 -0600

    Fix display for New Game warning popup

--- app/ui/popups/new-game-popup.tsx
+++ app/ui/popups/new-game-popup.tsx
@@ -10,9 +10,12 @@ export default function NewGamePopup(props: WinPopupProps) {
   return (
     <Modal isOpen={props.isOpen} onClose={props.onClose}>
       <div className="flex flex-col items-center justify-center px-12">
+        <h1 className="text-black text-4xl font-black my-4 ml-4">Warning</h1>
         <h2 className="text-black mb-8">{"Are you sure you want to generate a new game?"}</h2>
-        <GameButton text="Reset" onClick={() => window.location.reload()} />
-        <GameButton text="Reset" onClick={props.onClose} />
+        <div className="flex gap-2 mb-12">
+        <GameButton text="Reset" isAlert onClick={() => window.location.reload()} />
+        <GameButton text="Continue Current Game" onClick={props.onClose} />
+        </div>
       </div>
     </Modal>
   );
