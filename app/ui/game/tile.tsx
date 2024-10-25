5fe8a8b7fee4d5d39022956a10c5ca4e3e383ab2
Jacyked <services@jacykdev.com>
2024-10-24 21:33:42 -0600

    Trial new responsive sizing, not sure if I like it though.

--- app/ui/game/tile.tsx
+++ app/ui/game/tile.tsx
@@ -18,23 +18,19 @@ export default function Tile(props: TileProps) {
     const tColour = props.tileValue.selected ? 'text-blue-600' : 'text-black';
     const guessAnimation = props.animateGuess ? "transform -translate-y-2" : "";
     const wrongGuessAnimation = props.animateWrongGuess ? "animate-horizontal-shake" : "";
-    const tSize = props.tileValue.word.length <= 6 ? 'text-xs' : props.tileValue.word.length <= 8 ? 'text-xxs' : props.tileValue.word.length <= 10 ? 'text-xxxs' :'text-xxxxs'; 
+    const tSize = props.tileValue.word.length <= 5 ? 'text-s' : props.tileValue.word.length <= 7 ? 'text-xs' : props.tileValue.word.length <= 9 ? 'text-xxs' : 'text-xxxs'; 
     
     const handleClick = () => {
         props.onClick(props.tileValue);
       };
 
     return (
-      //<div className="rounded-xl bg-gray-50 p-2 shadow-sm">
-        //<div className="flex p-4">
-       // </div>
         <button
-            className={`${bColour} py-6 rounded-md break-all px-1 transition ease-in-out ${guessAnimation} ${wrongGuessAnimation}`} onClick={handleClick}
+            className={`${bColour} py-6 rounded-md break-all px-0 transition ease-in-out ${guessAnimation} ${wrongGuessAnimation}`} onClick={handleClick}
         >
             <h2 className={`${tColour} ${tSize} md:text-lg text-center font-bold`}>
                 {props.tileValue.word.toUpperCase()}
             </h2>
         </button>
-      //</div>
     );
   }
